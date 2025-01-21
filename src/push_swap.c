677c3123d243e04d3a2d3576141675ad28af2b39
nseon <nseon@student.42lyon.fr>
2025-01-21 18:07:02 +0100

    restart sort

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:44:26 by nseon             #+#    #+#             */
-/*   Updated: 2025/01/20 18:20:52 by nseon            ###   ########.fr       */
+/*   Updated: 2025/01/21 18:06:26 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,82 +17,68 @@
 
 void	pre_tri(t_stacks *stacks)
 {
-	int	count_red;
+	int	n;
 
-	count_red = 0;
+	n = stacks->sizet / 20;
 	while (stacks->sizea > 0)
 	{
-		if (stacks->b[0] < stacks->sizet / 3)
-			rb(stacks);
-		while (stacks->a[0] > 2 * (stacks->sizet / 3) && count_red < stacks->sizea)
+		while (stacks->sizea > 0 && (stacks->a[0] > stacks->sizet - n || stacks->a[0] < n))
 		{
-			ra(stacks);
-			count_red++;
-		}
-		pb(stacks);
-	}
-}
-
-void	sortpart(t_stacks *stacks)
-{
-	int	i;
-	int	j;
-
-	j = 0;
-	i = stacks->sizea - 1;
-	if (stacks->b[0] > stacks->a[i])
-	{
-		pa(stacks);
-		ra(stacks);
-	}
-	else if (stacks->b[0] < stacks->a[0])
-		pa(stacks);
-	else if (i > 0)
-	{
-		while (!(stacks->a[i - 1] <= stacks->b[0] && stacks->b[0] <= stacks->a[i]))
-		{
-			i--;
-		}
-		while (j < i)
-		{
-			ra(stacks);
-			j++;
-		}
-		pa(stacks);
-		while (j > 0)
-		{
-			rra(stacks);
-			if (stacks->a[stacks->sizea - 1])
-			j--;
-		}
-	}
-}
-
-void	sort(t_stacks *stacks)
-{
-	int	n;
-	int	count;
-
-	n = 6;
-	while (stacks->sizeb > 0)
-	{
-		count = 0;
-		while (stacks->b[0] >= ((n / 2) - 1) * stacks->sizet / 3 && count <= stacks->sizet / 6)
-		{
-			if (n * stacks->sizet / 6 >= stacks->b[0] && stacks->b[0] >= (n - 1) * stacks->sizet / 6)
-				sortpart(stacks);
-			else
+			if (stacks->a[0] > stacks->sizet - n)
 			{
+				pb(stacks);
 				rb(stacks);
-				count++;
+				n += 1;
+			}
+			if (stacks->a[0] < n)
+			{
+				pb(stacks);
 			}
 		}
-		while (stacks->b[stacks->sizeb - 1] >= ((n / 2) - 1) * stacks->sizet / 3 && stacks->sizeb > 0)
+		ra(stacks);
+	}
+}
+
+int	search_next(t_stacks *stacks)
+{
+	int	i;
+
+	i = 0;
+	while (stacks->b[i] != stacks->sizeb && i < stacks->sizeb)
+		i++;
+	return (i);
+}
+
+void	tri(t_stacks *stacks)
+{
+	int	n;
+
+	n = 0;
+	while (stacks->b[0] != stacks->sizet - n)
+	{
+		if (stacks->b[0] > stacks->a[stacks->sizea - 1])
 		{
-			rrb(stacks);
-			sortpart(stacks);
+			pa(stacks);
+			ra(stacks);
 		}
-		n -= 2;
+		rrb(stacks);
+	}
+	pa(stacks);
+	while (stacks->sizeb > 0)
+	{
+		while (search_next(stacks) >= stacks->sizeb)
+			ra(stacks);
+		if (search_next(stacks) <= stacks->sizeb / 2)
+		{
+			while (stacks->b[0] != stacks->sizeb)
+				rb(stacks);
+		}
+		else
+		{
+			while (stacks->b[0] != stacks->sizeb)
+				rrb(stacks);
+		}
+		pa(stacks);
 	}
 }
 
@@ -127,6 +113,6 @@ int	main(int argc, char **argv)
 		return (0);
 	fill_struct(argv + 1, argc - 1, stacks);
 	pre_tri(stacks);
-	sort(stacks);
+	tri(stacks);
 	return (0);
 }
